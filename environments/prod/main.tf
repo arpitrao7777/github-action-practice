77c7906d7221a75e4313e1bd343d04a688463f3b
Arpit <arpit32971@gmail.com>
2025-11-21 15:29:02 +0530

    prod environment added

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,16 +1,16 @@
-module "rg_dev" {
+module "rg_prod" {
   source = "../../modules/resource_group"
   rgs    = var.rgs
 }
 
-module "cr_dev" {
-  depends_on = [module.rg_dev]
+module "cr_prod" {
+  depends_on = [module.rg_prod]
   source     = "../../modules/container_registry"
   acrs       = var.acrs
 }
 
-module "aks_dev" {
-  depends_on = [module.rg_dev]
+module "aks_prod" {
+  depends_on = [module.rg_prod]
   source     = "../../modules/kubernetes_cluster"
   aks        = var.aks
 }
